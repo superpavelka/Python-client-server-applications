d1d0c7dffd63a5876aa8453de4579c00720a6f28
krilatiy <pos@list.ru>
2020-01-24 17:11:51 +0300

    les4: 4.1

---
+++ Task4/common/variables.py
@@ -0,0 +1,24 @@
+"""Константы"""
+
+# Порт поумолчанию для сетевого ваимодействия
+DEFAULT_PORT = 8000
+# IP адрес по умолчанию для подключения клиента
+DEFAULT_IP = '127.0.0.1'
+# Максимальная очередь подключений
+MAX_CONNECTIONS = 5
+# Максимальная длинна сообщения в байтах
+MAX_PACKAGE_LENGTH = 1024
+# Кодировка проекта
+ENCODING = 'utf-8'
+
+# Прококол JIM основные ключи:
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+LOGIN = 'login'
+
+# Прочие ключи, используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONSE_DEFAULT_IP = 'response_default_ip'
