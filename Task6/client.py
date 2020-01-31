7ee2d3e4bd434555b33a277d629292348aa3a5d3
krilatiy <pos@list.ru>
2020-01-31 18:55:03 +0300

    les6: 6.1,6.2

---
+++ Task6/client.py
@@ -0,0 +1,104 @@
+"""Программа-клиент"""
+
+import sys
+import json
+import socket
+import time
+import re
+import logging
+import logs.client_config
+from errors import ReqFieldMissingError
+from common.variables import ACTION, PRESENCE, TIME, USER, LOGIN, \
+    RESPONSE, ERROR, DEFAULT_IP, DEFAULT_PORT
+from common.utils import get_message, send_message
+from decorators import log_extra_info
+
+# Инициализация клиентского логера
+CLIENT_LOGGER = logging.getLogger('client')
+
+@log_extra_info
+def create_presence(login='Guest'):
+    '''
+    Функция генерирует запрос о присутствии клиента
+    :param login:
+    :return:
+    '''
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            LOGIN: login
+        }
+    }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {login}')
+    return out
+
+@log_extra_info
+def process_ans(message):
+    '''
+    Функция разбирает ответ сервера
+    :param message:
+    :return:
+    '''
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK - Запрос выполнен успешно'
+        return f'400 : {message[ERROR]}'
+    raise ReqFieldMissingError(RESPONSE)
+
+@log_extra_info
+def main():
+    '''Загружаем параметы коммандной строки'''
+    if '-a' in sys.argv:
+        server_address = sys.argv[sys.argv.index('-a') + 1]
+    else:
+        server_address = DEFAULT_IP
+        CLIENT_LOGGER.info(f'Не найден параметр -\'a\'.'
+                           f'Установлено стандартное значение IP адреса: {server_address}')
+    if '-p' in sys.argv:
+        server_port = int(sys.argv[sys.argv.index('-p') + 1])
+    else:
+        server_port = DEFAULT_PORT
+        CLIENT_LOGGER.info(f'Не найден параметр -\'p\'.'
+                           f'Установлено стандартное значение порта: {server_port}')
+    try:
+        # Проверка IP адреса, если не соответсвует регулярке то выкинет исключение AttributeError
+        [0 <= int(x) < 256 for x in
+         re.split('\.', re.match(r'^\d+\.\d+\.\d+\.\d+$', server_address).group(0))].count(True) == 4
+        if server_port < 1024 or server_port > 65535:
+            raise AttributeError
+
+    except AttributeError:
+        server_address = DEFAULT_IP
+        server_port = DEFAULT_PORT
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта или IP адреса.'
+            f'Установлены стандартные значения для '
+            f'подключения {server_port},{server_address}')
+
+    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
+                       f'адрес сервера: {server_address} , порт: {server_port}')
+    # Инициализация сокета и обмен
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        transport.connect((server_address, server_port))
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                               f'конечный компьютер отверг запрос на подключение.')
+        sys.exit(1)
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
+    except ReqFieldMissingError as error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{error.missing_field}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ Task6/server.py
@@ -0,0 +1,114 @@
+"""Программа-сервер"""
+
+import socket
+import sys
+import json
+import re
+import logging
+import logs.server_config
+from common.variables import ACTION, LOGIN, RESPONSE, MAX_CONNECTIONS, \
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, DEFAULT_IP
+from common.utils import get_message, send_message
+from errors import IncorrectDataRecivedError
+from decorators import log_extra_info
+
+# Инициализация логирования сервера.
+SERVER_LOGGER = logging.getLogger('server')
+
+@log_extra_info
+def process_client_message(message):
+    '''
+    Обработчик сообщений от клиентов, принимает словарь -
+    сообщение от клинта, проверяет корректность,
+    возвращает словарь-ответ для клиента
+
+    :param message:
+    :return:
+    '''
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][LOGIN] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request - Сервер не понимает запрос из-за неверного синтаксиса'
+    }
+
+@log_extra_info
+def main():
+    '''
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    Сначала обрабатываем порт:
+    server.py -p 8000 -a 127.0.0.1
+    :return:
+    '''
+
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                                   f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+            sys.exit(1)
+    except IndexError:
+        SERVER_LOGGER.critical(f'После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+
+    # Затем загружаем какой адрес слушать
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = DEFAULT_IP
+
+    except IndexError:
+        SERVER_LOGGER.critical('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+    # Проверка IP адреса, если не соответсвует регулярке то выкинет исключение AttributeError
+    try:
+        [0 <= int(x) < 256 for x in
+         re.split('\.', re.match(r'^\d+\.\d+\.\d+\.\d+$', listen_address).group(0))].count(True) == 4
+
+    except AttributeError:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего IP адреса '
+                               f'{listen_address}.')
+        sys.exit(1)
+
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}.')
+
+    # Готовим сокет
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соедение с компьютером {client_address}')
+        try:
+            message_from_client = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
+            response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
+            send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать Json строку, полученную от '
+                                f'клиента {client_address}. Соединение закрывается.')
+            client.close()
+        except IncorrectDataRecivedError:
+            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
+                                f'Соединение закрывается.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
